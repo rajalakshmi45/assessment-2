d0b1016eccf1378ba11bcbae830f956c3e3b33cf
Rajalakshmi S <rajals17@in.ibm.com>
2019-07-24 11:22:41 +0530

    final commit

---
+++ product/db/users.js
@@ -0,0 +1,21 @@
+const product = [
+    {
+        product_id : 1,
+        product_name : "washing machine",
+        product_price : 25000,
+        product_quantity : 10
+    },
+    {
+        product_id : 2,
+        product_name : "fridge",
+        product_price : 10000,
+        product_quantity : 20
+    },
+    {
+        product_id : 3,
+        product_name : "microwave oven",
+        product_price : 1500,
+        product_quantity : 15
+    }
+]
+module.exports.product = product;
